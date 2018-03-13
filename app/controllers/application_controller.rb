b773cb56d6ef2042a960fa820f0a77b6a5721ae2
Ray Blasko <mray6288@gmail.com>
2018-03-13 11:17:19 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+    	erb :super_hero
+    end
+
+    post '/teams' do
+    	@team = Team.new(params[:team][:name], params[:team][:motto])
+    	params[:team][:members].each do |hero|
+    		@team.members << Superhero.new(hero[:name], hero[:power], hero[:bio], @team)
+    	end
+    	erb :team
+    end
 
 end
---
+++ app/models/team.rb
@@ -0,0 +1,12 @@
+class Team
+	attr_accessor :name, :motto, :members
+
+	def initialize(name, motto)
+		@name = name
+		@motto = motto
+		@members = []
+	end
+
+
+
+end
